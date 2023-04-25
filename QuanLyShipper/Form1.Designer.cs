282f81da696d70ab89ff23f566fe9e941d864b65
cowcut <112707238+cowcut@users.noreply.github.com>
2023-04-26 01:53:53 +0700

    giao dien hoàn chỉnh

--- QuanLyShipper/Form1.Designer.cs
+++ QuanLyShipper/Form1.Designer.cs
@@ -33,15 +33,25 @@ namespace QuanLyShipper
             this.QuanLy_btn = new System.Windows.Forms.Button();
             this.Shipper_btn = new System.Windows.Forms.Button();
             this.Main_pnl = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TaiKhoan_lbl = new System.Windows.Forms.Label();
+            this.MatKhau_lbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TaiKhoan_txt = new System.Windows.Forms.TextBox();
+            this.MatKhau_txt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.DangNhap_btn = new System.Windows.Forms.Button();
             this.Main_pnl.SuspendLayout();
             this.SuspendLayout();
             // 
             // NguoiDung_btn
             // 
             this.NguoiDung_btn.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.NguoiDung_btn.Location = new System.Drawing.Point(130, 183);
+            this.NguoiDung_btn.Location = new System.Drawing.Point(0, 0);
+            this.NguoiDung_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.NguoiDung_btn.Name = "NguoiDung_btn";
-            this.NguoiDung_btn.Size = new System.Drawing.Size(112, 33);
+            this.NguoiDung_btn.Size = new System.Drawing.Size(149, 41);
             this.NguoiDung_btn.TabIndex = 0;
             this.NguoiDung_btn.Text = "Người dùng";
             this.NguoiDung_btn.UseVisualStyleBackColor = true;
@@ -50,9 +60,10 @@ namespace QuanLyShipper
             // QuanLy_btn
             // 
             this.QuanLy_btn.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.QuanLy_btn.Location = new System.Drawing.Point(470, 183);
+            this.QuanLy_btn.Location = new System.Drawing.Point(0, 98);
+            this.QuanLy_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.QuanLy_btn.Name = "QuanLy_btn";
-            this.QuanLy_btn.Size = new System.Drawing.Size(112, 33);
+            this.QuanLy_btn.Size = new System.Drawing.Size(149, 41);
             this.QuanLy_btn.TabIndex = 0;
             this.QuanLy_btn.Text = "Người quản lý";
             this.QuanLy_btn.UseVisualStyleBackColor = true;
@@ -61,9 +72,10 @@ namespace QuanLyShipper
             // Shipper_btn
             // 
             this.Shipper_btn.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.Shipper_btn.Location = new System.Drawing.Point(304, 183);
+            this.Shipper_btn.Location = new System.Drawing.Point(0, 35);
+            this.Shipper_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Shipper_btn.Name = "Shipper_btn";
-            this.Shipper_btn.Size = new System.Drawing.Size(112, 33);
+            this.Shipper_btn.Size = new System.Drawing.Size(149, 41);
             this.Shipper_btn.TabIndex = 0;
             this.Shipper_btn.Text = "Shipper";
             this.Shipper_btn.UseVisualStyleBackColor = true;
@@ -74,23 +86,122 @@ namespace QuanLyShipper
             this.Main_pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.Main_pnl.Controls.Add(this.DangNhap_btn);
+            this.Main_pnl.Controls.Add(this.comboBox1);
+            this.Main_pnl.Controls.Add(this.MatKhau_txt);
+            this.Main_pnl.Controls.Add(this.TaiKhoan_txt);
+            this.Main_pnl.Controls.Add(this.label5);
+            this.Main_pnl.Controls.Add(this.MatKhau_lbl);
+            this.Main_pnl.Controls.Add(this.TaiKhoan_lbl);
+            this.Main_pnl.Controls.Add(this.label2);
+            this.Main_pnl.Controls.Add(this.label1);
             this.Main_pnl.Controls.Add(this.Shipper_btn);
             this.Main_pnl.Controls.Add(this.QuanLy_btn);
             this.Main_pnl.Controls.Add(this.NguoiDung_btn);
             this.Main_pnl.Location = new System.Drawing.Point(-1, 0);
+            this.Main_pnl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Main_pnl.Name = "Main_pnl";
-            this.Main_pnl.Size = new System.Drawing.Size(800, 439);
+            this.Main_pnl.Size = new System.Drawing.Size(1067, 540);
             this.Main_pnl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(385, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Đăng Nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // TaiKhoan_lbl
+            // 
+            this.TaiKhoan_lbl.AutoSize = true;
+            this.TaiKhoan_lbl.Location = new System.Drawing.Point(291, 228);
+            this.TaiKhoan_lbl.Name = "TaiKhoan_lbl";
+            this.TaiKhoan_lbl.Size = new System.Drawing.Size(68, 16);
+            this.TaiKhoan_lbl.TabIndex = 2;
+            this.TaiKhoan_lbl.Text = "Tài Khoản";
+            this.TaiKhoan_lbl.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // MatKhau_lbl
+            // 
+            this.MatKhau_lbl.AutoSize = true;
+            this.MatKhau_lbl.Location = new System.Drawing.Point(297, 308);
+            this.MatKhau_lbl.Name = "MatKhau_lbl";
+            this.MatKhau_lbl.Size = new System.Drawing.Size(62, 16);
+            this.MatKhau_lbl.TabIndex = 3;
+            this.MatKhau_lbl.Text = "Mật Khẩu";
+            this.MatKhau_lbl.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Loại Tài Khoản";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // TaiKhoan_txt
+            // 
+            this.TaiKhoan_txt.Location = new System.Drawing.Point(294, 247);
+            this.TaiKhoan_txt.Name = "TaiKhoan_txt";
+            this.TaiKhoan_txt.Size = new System.Drawing.Size(325, 22);
+            this.TaiKhoan_txt.TabIndex = 6;
+            // 
+            // MatKhau_txt
+            // 
+            this.MatKhau_txt.Location = new System.Drawing.Point(294, 327);
+            this.MatKhau_txt.Name = "MatKhau_txt";
+            this.MatKhau_txt.Size = new System.Drawing.Size(325, 22);
+            this.MatKhau_txt.TabIndex = 7;
+            this.MatKhau_txt.UseSystemPasswordChar = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Quản Lý",
+            "Shipper",
+            "Khách Hàng"});
+            this.comboBox1.Location = new System.Drawing.Point(294, 180);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(325, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // DangNhap_btn
+            // 
+            this.DangNhap_btn.Location = new System.Drawing.Point(294, 390);
+            this.DangNhap_btn.Name = "DangNhap_btn";
+            this.DangNhap_btn.Size = new System.Drawing.Size(161, 50);
+            this.DangNhap_btn.TabIndex = 9;
+            this.DangNhap_btn.Text = "Đăng Nhập";
+            this.DangNhap_btn.UseVisualStyleBackColor = true;
+            this.DangNhap_btn.Click += new System.EventHandler(this.DangNhap_btn_Click);
             // 
             // frm_HeyYou
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 437);
+            this.ClientSize = new System.Drawing.Size(1064, 538);
             this.Controls.Add(this.Main_pnl);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_HeyYou";
             this.Text = "HeyYou";
             this.Main_pnl.ResumeLayout(false);
+            this.Main_pnl.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -101,6 +212,15 @@ namespace QuanLyShipper
         private System.Windows.Forms.Button QuanLy_btn;
         private System.Windows.Forms.Button Shipper_btn;
         private System.Windows.Forms.Panel Main_pnl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label MatKhau_lbl;
+        private System.Windows.Forms.Label TaiKhoan_lbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox MatKhau_txt;
+        private System.Windows.Forms.TextBox TaiKhoan_txt;
+        private System.Windows.Forms.Button DangNhap_btn;
     }
 }
 
