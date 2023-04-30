5dfa98bd189b158279bf5f782889b339c7f1f75a
hltminh710 <131602332+hltminh710@users.noreply.github.com>
2023-04-30 13:47:47 +0700

    Add files via upload

---
+++ QuanLyShipper/Taikhoanshipper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyShipper
+{
+    public partial class Taikhoanshipper : Form
+    {
+        public Taikhoanshipper()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
