7b94381e4b133707acfc792da5880650125aca05
Duong DD <92495354+FE-BAPPP@users.noreply.github.com>
2023-05-06 15:59:53 +0700

    Add files via upload

---
+++ QuanLyShipper/QuanLyShipper/frm_HeyYou.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyShipper
+{
+    public partial class frm_HeyYou : Form
+    {
+        public frm_HeyYou()
+        {
+            InitializeComponent();
+        }
+
+        private void DangNhap_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frm_HeyYou_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_pnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
