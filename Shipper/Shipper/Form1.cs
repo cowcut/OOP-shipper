9f523e6dd62a9176acb97176241003850caac60b
cowcut <112707238+cowcut@users.noreply.github.com>
2023-05-28 16:23:11 +0700

    Add files via upload

---
+++ Shipper/Shipper/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Shipper
+{
+    public partial class frm_shipper : Form
+    {
+        private Button currentButton;
+        private Form activeForm;
+        public frm_shipper()
+        {
+            InitializeComponent();
+            btn_exit.Visible = false;
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = Color.FromArgb(204, 131, 248);
+                    currentButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+                    currentButton.Font = new Font("Microsoft Sans Serif", 12.5F, FontStyle.Regular, GraphicsUnit.Point);
+                    btn_exit.Visible = true;
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentButton != null)
+            {
+                currentButton.BackColor = Color.FromArgb(165, 107, 225);
+                currentButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentButton.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.paneldesktop.Controls.Add(childForm);
+            this.paneldesktop.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lbl_trangchu.Text = childForm.Text;
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_taikhoan_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.formTaiKhoan(), sender);
+        }
+
+        private void btn_donhang_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.formDonHang(), sender);
+        }
+
+        private void btn_vanchuyen_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.formVanChuyen(), sender);
+        }
+
+        private void btn_thongbao_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.formThongBao(), sender);
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+                Reset();
+            }
+        }
+
+        private void Reset()
+        {
+            DisableButton();
+            lbl_trangchu.Text = "Trang chủ";
+            panel6.BackColor = Color.FromArgb(165, 107, 225);
+            currentButton = null;
+            btn_exit.Visible = false;
+
+        }
+
+        private void btn_dangxuat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Shipper/Shipper/Forms/formTaiKhoan.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace Shipper.Forms
+{
+    partial class formTaiKhoan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            rjPanel2 = new QuanLyShipper.RJControls.RJPanel();
+            rjPanel3 = new QuanLyShipper.RJControls.RJPanel();
+            rjLogin1 = new QuanLyShipper.RJControls.RJLogin();
+            panel2 = new Panel();
+            rjPanel1 = new QuanLyShipper.RJControls.RJPanel();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            rjButton6 = new QuanLyShipper.RJControls.RJButton();
+            rjButton5 = new QuanLyShipper.RJControls.RJButton();
+            rjButton4 = new QuanLyShipper.RJControls.RJButton();
+            rjButton3 = new QuanLyShipper.RJControls.RJButton();
+            rjButton2 = new QuanLyShipper.RJControls.RJButton();
+            rjButton1 = new QuanLyShipper.RJControls.RJButton();
+            panel1.SuspendLayout();
+            rjPanel2.SuspendLayout();
+            panel2.SuspendLayout();
+            rjPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(rjPanel2);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(448, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(255, 434);
+            panel1.TabIndex = 1;
+            // 
+            // rjPanel2
+            // 
+            rjPanel2.BackColor = Color.FromArgb(165, 107, 225);
+            rjPanel2.BackgroundColor = Color.FromArgb(165, 107, 225);
+            rjPanel2.BorderColor = Color.PaleVioletRed;
+            rjPanel2.BorderRadius = 0;
+            rjPanel2.BorderSize = 0;
+            rjPanel2.Controls.Add(rjPanel3);
+            rjPanel2.Controls.Add(rjLogin1);
+            rjPanel2.Dock = DockStyle.Fill;
+            rjPanel2.ForeColor = Color.White;
+            rjPanel2.Location = new Point(0, 0);
+            rjPanel2.Name = "rjPanel2";
+            rjPanel2.Size = new Size(255, 434);
+            rjPanel2.TabIndex = 2;
+            rjPanel2.TextColor = Color.White;
+            // 
+            // rjPanel3
+            // 
+            rjPanel3.BackColor = Color.White;
+            rjPanel3.BackgroundColor = Color.White;
+            rjPanel3.BackgroundImage = Properties.Resources.Avatar;
+            rjPanel3.BackgroundImageLayout = ImageLayout.Stretch;
+            rjPanel3.BorderColor = Color.PaleVioletRed;
+            rjPanel3.BorderRadius = 60;
+            rjPanel3.BorderSize = 0;
+            rjPanel3.ForeColor = Color.White;
+            rjPanel3.Location = new Point(64, 29);
+            rjPanel3.Name = "rjPanel3";
+            rjPanel3.Size = new Size(123, 125);
+            rjPanel3.TabIndex = 2;
+            rjPanel3.TextColor = Color.White;
+            // 
+            // rjLogin1
+            // 
+            rjLogin1.BackColor = Color.RoyalBlue;
+            rjLogin1.BackgroundColor = Color.RoyalBlue;
+            rjLogin1.BorderColor = Color.PaleVioletRed;
+            rjLogin1.BorderRadius = 5;
+            rjLogin1.BorderSize = 0;
+            rjLogin1.FlatAppearance.BorderSize = 0;
+            rjLogin1.FlatStyle = FlatStyle.Flat;
+            rjLogin1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            rjLogin1.ForeColor = Color.White;
+            rjLogin1.Location = new Point(55, 210);
+            rjLogin1.Name = "rjLogin1";
+            rjLogin1.Size = new Size(150, 40);
+            rjLogin1.TabIndex = 1;
+            rjLogin1.Text = "Đổi mật khẩu";
+            rjLogin1.TextColor = Color.White;
+            rjLogin1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(rjPanel1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(448, 434);
+            panel2.TabIndex = 2;
+            // 
+            // rjPanel1
+            // 
+            rjPanel1.BackColor = Color.MediumPurple;
+            rjPanel1.BackgroundColor = Color.MediumPurple;
+            rjPanel1.BorderColor = Color.White;
+            rjPanel1.BorderRadius = 0;
+            rjPanel1.BorderSize = 4;
+            rjPanel1.Controls.Add(label6);
+            rjPanel1.Controls.Add(label5);
+            rjPanel1.Controls.Add(label4);
+            rjPanel1.Controls.Add(label3);
+            rjPanel1.Controls.Add(label2);
+            rjPanel1.Controls.Add(label1);
+            rjPanel1.Controls.Add(rjButton6);
+            rjPanel1.Controls.Add(rjButton5);
+            rjPanel1.Controls.Add(rjButton4);
+            rjPanel1.Controls.Add(rjButton3);
+            rjPanel1.Controls.Add(rjButton2);
+            rjPanel1.Controls.Add(rjButton1);
+            rjPanel1.Dock = DockStyle.Fill;
+            rjPanel1.ForeColor = Color.White;
+            rjPanel1.Location = new Point(0, 0);
+            rjPanel1.Name = "rjPanel1";
+            rjPanel1.Size = new Size(448, 434);
+            rjPanel1.TabIndex = 1;
+            rjPanel1.TextColor = Color.White;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(18, 286);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 21);
+            label6.TabIndex = 11;
+            label6.Text = "Email :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(18, 348);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 21);
+            label5.TabIndex = 10;
+            label5.Text = "Địa chỉ :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(18, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 21);
+            label4.TabIndex = 9;
+            label4.Text = "Tên :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(18, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 21);
+            label3.TabIndex = 8;
+            label3.Text = "SĐT :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(18, 225);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 21);
+            label2.TabIndex = 7;
+            label2.Text = "CCCD :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(13, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 21);
+            label1.TabIndex = 6;
+            label1.Text = "Tên tài khoản :";
+            // 
+            // rjButton6
+            // 
+            rjButton6.BackColor = SystemColors.Window;
+            rjButton6.BorderColor = Color.DodgerBlue;
+            rjButton6.BorderFocusColor = Color.Crimson;
+            rjButton6.BorderRadius = 15;
+            rjButton6.BorderSize = 2;
+            rjButton6.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjButton6.ForeColor = Color.FromArgb(64, 64, 64);
+            rjButton6.Location = new Point(162, 343);
+            rjButton6.Margin = new Padding(4);
+            rjButton6.Multiline = false;
+            rjButton6.Name = "rjButton6";
+            rjButton6.Padding = new Padding(10, 7, 10, 7);
+            rjButton6.PasswordChar = false;
+            rjButton6.PlaceholderColor = Color.DarkGray;
+            rjButton6.PlaceholderText = "";
+            rjButton6.Size = new Size(260, 31);
+            rjButton6.TabIndex = 5;
+            rjButton6.Texts = "";
+            rjButton6.UnderlinedStyle = false;
+            // 
+            // rjButton5
+            // 
+            rjButton5.BackColor = SystemColors.Window;
+            rjButton5.BorderColor = Color.DodgerBlue;
+            rjButton5.BorderFocusColor = Color.Crimson;
+            rjButton5.BorderRadius = 15;
+            rjButton5.BorderSize = 2;
+            rjButton5.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjButton5.ForeColor = Color.FromArgb(64, 64, 64);
+            rjButton5.Location = new Point(162, 96);
+            rjButton5.Margin = new Padding(4);
+            rjButton5.Multiline = false;
+            rjButton5.Name = "rjButton5";
+            rjButton5.Padding = new Padding(10, 7, 10, 7);
+            rjButton5.PasswordChar = false;
+            rjButton5.PlaceholderColor = Color.DarkGray;
+            rjButton5.PlaceholderText = "";
+            rjButton5.Size = new Size(260, 31);
+            rjButton5.TabIndex = 4;
+            rjButton5.Texts = "";
+            rjButton5.UnderlinedStyle = false;
+            // 
+            // rjButton4
+            // 
+            rjButton4.BackColor = SystemColors.Window;
+            rjButton4.BorderColor = Color.DodgerBlue;
+            rjButton4.BorderFocusColor = Color.Crimson;
+            rjButton4.BorderRadius = 15;
+            rjButton4.BorderSize = 2;
+            rjButton4.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjButton4.ForeColor = Color.FromArgb(64, 64, 64);
+            rjButton4.Location = new Point(162, 157);
+            rjButton4.Margin = new Padding(4);
+            rjButton4.Multiline = false;
+            rjButton4.Name = "rjButton4";
+            rjButton4.Padding = new Padding(10, 7, 10, 7);
+            rjButton4.PasswordChar = false;
+            rjButton4.PlaceholderColor = Color.DarkGray;
+            rjButton4.PlaceholderText = "";
+            rjButton4.Size = new Size(260, 31);
+            rjButton4.TabIndex = 3;
+            rjButton4.Texts = "";
+            rjButton4.UnderlinedStyle = false;
+            // 
+            // rjButton3
+            // 
+            rjButton3.BackColor = SystemColors.Window;
+            rjButton3.BorderColor = Color.DodgerBlue;
+            rjButton3.BorderFocusColor = Color.Crimson;
+            rjButton3.BorderRadius = 15;
+            rjButton3.BorderSize = 2;
+            rjButton3.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjButton3.ForeColor = Color.FromArgb(64, 64, 64);
+            rjButton3.Location = new Point(162, 281);
+            rjButton3.Margin = new Padding(4);
+            rjButton3.Multiline = false;
+            rjButton3.Name = "rjButton3";
+            rjButton3.Padding = new Padding(10, 7, 10, 7);
+            rjButton3.PasswordChar = false;
+            rjButton3.PlaceholderColor = Color.DarkGray;
+            rjButton3.PlaceholderText = "";
+            rjButton3.Size = new Size(260, 31);
+            rjButton3.TabIndex = 2;
+            rjButton3.Texts = "";
+            rjButton3.UnderlinedStyle = false;
+            // 
+            // rjButton2
+            // 
+            rjButton2.BackColor = SystemColors.Window;
+            rjButton2.BorderColor = Color.DodgerBlue;
+            rjButton2.BorderFocusColor = Color.Crimson;
+            rjButton2.BorderRadius = 15;
+            rjButton2.BorderSize = 2;
+            rjButton2.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjButton2.ForeColor = Color.FromArgb(64, 64, 64);
+            rjButton2.Location = new Point(162, 220);
+            rjButton2.Margin = new Padding(4);
+            rjButton2.Multiline = false;
+            rjButton2.Name = "rjButton2";
+            rjButton2.Padding = new Padding(10, 7, 10, 7);
+            rjButton2.PasswordChar = false;
+            rjButton2.PlaceholderColor = Color.DarkGray;
+            rjButton2.PlaceholderText = "";
+            rjButton2.Size = new Size(260, 31);
+            rjButton2.TabIndex = 1;
+            rjButton2.Texts = "";
+            rjButton2.UnderlinedStyle = false;
+            // 
+            // rjButton1
+            // 
+            rjButton1.BackColor = SystemColors.Window;
+            rjButton1.BorderColor = Color.DodgerBlue;
+            rjButton1.BorderFocusColor = Color.Crimson;
+            rjButton1.BorderRadius = 15;
+            rjButton1.BorderSize = 2;
+            rjButton1.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point);
+            rjButton1.ForeColor = Color.FromArgb(64, 64, 64);
+            rjButton1.Location = new Point(162, 41);
+            rjButton1.Margin = new Padding(4);
+            rjButton1.Multiline = false;
+            rjButton1.Name = "rjButton1";
+            rjButton1.Padding = new Padding(10, 7, 10, 7);
+            rjButton1.PasswordChar = false;
+            rjButton1.PlaceholderColor = Color.DarkGray;
+            rjButton1.PlaceholderText = "";
+            rjButton1.Size = new Size(260, 31);
+            rjButton1.TabIndex = 0;
+            rjButton1.Texts = "";
+            rjButton1.UnderlinedStyle = false;
+            // 
+            // formTaiKhoan
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(703, 434);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "formTaiKhoan";
+            Text = "Tài khoản";
+            panel1.ResumeLayout(false);
+            rjPanel2.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            rjPanel1.ResumeLayout(false);
+            rjPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private QuanLyShipper.RJControls.RJPanel rjPanel2;
+        private QuanLyShipper.RJControls.RJPanel rjPanel3;
+        private QuanLyShipper.RJControls.RJLogin rjLogin1;
+        private Panel panel2;
+        private QuanLyShipper.RJControls.RJPanel rjPanel1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private QuanLyShipper.RJControls.RJButton rjButton6;
+        private QuanLyShipper.RJControls.RJButton rjButton5;
+        private QuanLyShipper.RJControls.RJButton rjButton4;
+        private QuanLyShipper.RJControls.RJButton rjButton3;
+        private QuanLyShipper.RJControls.RJButton rjButton2;
+        private QuanLyShipper.RJControls.RJButton rjButton1;
+    }
+}
