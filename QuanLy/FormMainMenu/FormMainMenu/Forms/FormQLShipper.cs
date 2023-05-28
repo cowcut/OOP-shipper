13316a639e667bd12d13a6cbc4e5c6d18f4b4ee1
cowcut <112707238+cowcut@users.noreply.github.com>
2023-05-28 16:03:16 +0700

    Add files via upload

---
+++ QuanLy/FormMainMenu/FormMainMenu/Forms/FormQLShipper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMainMenu.Forms
+{
+    public partial class FormQLShipper : Form
+    {
+        public FormQLShipper()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void rjButton3__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjLogin4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjButton1__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void rjPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
