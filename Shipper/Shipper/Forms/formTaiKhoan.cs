26d21affc77df5ec0f2cb388155e4936d8a9ea62
cowcut <112707238+cowcut@users.noreply.github.com>
2023-05-28 16:20:48 +0700

    Add files via upload

---
+++ Shipper/Shipper/Forms/formTaiKhoan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shipper.Forms
+{
+    public partial class formTaiKhoan : Form
+    {
+        public formTaiKhoan()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
