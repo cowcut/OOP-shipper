85c74488b0390d18f008e04fb9b1681f9e0d088b
cowcut <112707238+cowcut@users.noreply.github.com>
2023-05-29 12:45:59 +0700

    Add files via upload

---
+++ DangNhap/QuanLyShipper/QuanLyShipper (1)/frm_HeyYou.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyShipper
+{
+    public partial class frm_HeyYou : Form
+    {
+        public frm_HeyYou()
+        {
+            InitializeComponent();
+        }
+
+        private void DangNhap_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frm_HeyYou_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_pnl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MatKhau_txt__TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjLogin4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
