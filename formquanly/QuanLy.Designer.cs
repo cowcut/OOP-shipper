c2387aecab1794602c0b0dfcad5b8ae734ffaeac
ThgCH <91693025+chan-gio@users.noreply.github.com>
2023-05-31 21:36:41 +0700

    Add files via upload

---
+++ formquanly/QuanLy.Designer.cs
@@ -0,0 +1,282 @@
+﻿using System;
+
+namespace FormMainMenu
+{
+    partial class QuanLy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDesktop = new QuanLyShipper.RJControls.RJPanel();
+            this.panelTitleBar = new QuanLyShipper.RJControls.RJPanel();
+            this.btnCloseChildForm = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelMenu = new QuanLyShipper.RJControls.RJPanel();
+            this.btn_dangxuat = new QuanLyShipper.RJControls.RJLogin();
+            this.btnCaiDat = new QuanLyShipper.RJControls.RJLogin();
+            this.btnThongBao = new QuanLyShipper.RJControls.RJLogin();
+            this.btnPBShipper = new QuanLyShipper.RJControls.RJLogin();
+            this.btnQLShipper = new QuanLyShipper.RJControls.RJLogin();
+            this.btnProducts = new QuanLyShipper.RJControls.RJLogin();
+            this.panelLogo = new QuanLyShipper.RJControls.RJPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTitleBar.SuspendLayout();
+            this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelDesktop.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelDesktop.BackgroundImage = global::QuanLyShipper.Properties.Resources.Brown_and_Yellow_Minimalist_Home_Sweet_Home_Delivery_Facebook_Cover__2_;
+            this.panelDesktop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelDesktop.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.panelDesktop.BorderRadius = 15;
+            this.panelDesktop.BorderSize = 3;
+            this.panelDesktop.ForeColor = System.Drawing.Color.White;
+            this.panelDesktop.Location = new System.Drawing.Point(220, 80);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(1123, 570);
+            this.panelDesktop.TabIndex = 2;
+            this.panelDesktop.TabStop = true;
+            this.panelDesktop.TextColor = System.Drawing.Color.White;
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.MediumPurple;
+            this.panelTitleBar.BackgroundColor = System.Drawing.Color.MediumPurple;
+            this.panelTitleBar.BorderColor = System.Drawing.Color.DarkOrchid;
+            this.panelTitleBar.BorderRadius = 20;
+            this.panelTitleBar.BorderSize = 0;
+            this.panelTitleBar.Controls.Add(this.btnCloseChildForm);
+            this.panelTitleBar.Controls.Add(this.lblTitle);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.ForeColor = System.Drawing.Color.White;
+            this.panelTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(1123, 80);
+            this.panelTitleBar.TabIndex = 1;
+            this.panelTitleBar.TextColor = System.Drawing.Color.White;
+            // 
+            // btnCloseChildForm
+            // 
+            this.btnCloseChildForm.Location = new System.Drawing.Point(0, 0);
+            this.btnCloseChildForm.Name = "btnCloseChildForm";
+            this.btnCloseChildForm.Size = new System.Drawing.Size(75, 23);
+            this.btnCloseChildForm.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(100, 23);
+            this.lblTitle.TabIndex = 1;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(28)))), ((int)(((byte)(63)))));
+            this.panelMenu.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(28)))), ((int)(((byte)(63)))));
+            this.panelMenu.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.panelMenu.BorderRadius = 20;
+            this.panelMenu.BorderSize = 0;
+            this.panelMenu.Controls.Add(this.btn_dangxuat);
+            this.panelMenu.Controls.Add(this.btnCaiDat);
+            this.panelMenu.Controls.Add(this.btnThongBao);
+            this.panelMenu.Controls.Add(this.btnPBShipper);
+            this.panelMenu.Controls.Add(this.btnQLShipper);
+            this.panelMenu.Controls.Add(this.btnProducts);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.ForeColor = System.Drawing.Color.White;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 650);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.TextColor = System.Drawing.Color.White;
+            // 
+            // btn_dangxuat
+            // 
+            this.btn_dangxuat.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_dangxuat.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_dangxuat.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_dangxuat.BorderRadius = 0;
+            this.btn_dangxuat.BorderSize = 0;
+            this.btn_dangxuat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dangxuat.ForeColor = System.Drawing.Color.White;
+            this.btn_dangxuat.Location = new System.Drawing.Point(0, 0);
+            this.btn_dangxuat.Name = "btn_dangxuat";
+            this.btn_dangxuat.Size = new System.Drawing.Size(150, 40);
+            this.btn_dangxuat.TabIndex = 0;
+            this.btn_dangxuat.TextColor = System.Drawing.Color.White;
+            this.btn_dangxuat.UseVisualStyleBackColor = false;
+            // 
+            // btnCaiDat
+            // 
+            this.btnCaiDat.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnCaiDat.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnCaiDat.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnCaiDat.BorderRadius = 0;
+            this.btnCaiDat.BorderSize = 0;
+            this.btnCaiDat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCaiDat.ForeColor = System.Drawing.Color.White;
+            this.btnCaiDat.Location = new System.Drawing.Point(0, 0);
+            this.btnCaiDat.Name = "btnCaiDat";
+            this.btnCaiDat.Size = new System.Drawing.Size(150, 40);
+            this.btnCaiDat.TabIndex = 1;
+            this.btnCaiDat.TextColor = System.Drawing.Color.White;
+            this.btnCaiDat.UseVisualStyleBackColor = false;
+            // 
+            // btnThongBao
+            // 
+            this.btnThongBao.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnThongBao.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnThongBao.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnThongBao.BorderRadius = 0;
+            this.btnThongBao.BorderSize = 0;
+            this.btnThongBao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThongBao.ForeColor = System.Drawing.Color.White;
+            this.btnThongBao.Location = new System.Drawing.Point(0, 0);
+            this.btnThongBao.Name = "btnThongBao";
+            this.btnThongBao.Size = new System.Drawing.Size(150, 40);
+            this.btnThongBao.TabIndex = 2;
+            this.btnThongBao.TextColor = System.Drawing.Color.White;
+            this.btnThongBao.UseVisualStyleBackColor = false;
+            // 
+            // btnPBShipper
+            // 
+            this.btnPBShipper.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnPBShipper.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnPBShipper.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnPBShipper.BorderRadius = 0;
+            this.btnPBShipper.BorderSize = 0;
+            this.btnPBShipper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPBShipper.ForeColor = System.Drawing.Color.White;
+            this.btnPBShipper.Location = new System.Drawing.Point(0, 0);
+            this.btnPBShipper.Name = "btnPBShipper";
+            this.btnPBShipper.Size = new System.Drawing.Size(150, 40);
+            this.btnPBShipper.TabIndex = 3;
+            this.btnPBShipper.TextColor = System.Drawing.Color.White;
+            this.btnPBShipper.UseVisualStyleBackColor = false;
+            // 
+            // btnQLShipper
+            // 
+            this.btnQLShipper.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnQLShipper.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnQLShipper.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnQLShipper.BorderRadius = 0;
+            this.btnQLShipper.BorderSize = 0;
+            this.btnQLShipper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQLShipper.ForeColor = System.Drawing.Color.White;
+            this.btnQLShipper.Location = new System.Drawing.Point(0, 0);
+            this.btnQLShipper.Name = "btnQLShipper";
+            this.btnQLShipper.Size = new System.Drawing.Size(150, 40);
+            this.btnQLShipper.TabIndex = 4;
+            this.btnQLShipper.TextColor = System.Drawing.Color.White;
+            this.btnQLShipper.UseVisualStyleBackColor = false;
+            // 
+            // btnProducts
+            // 
+            this.btnProducts.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnProducts.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnProducts.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnProducts.BorderRadius = 0;
+            this.btnProducts.BorderSize = 0;
+            this.btnProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProducts.ForeColor = System.Drawing.Color.White;
+            this.btnProducts.Location = new System.Drawing.Point(0, 0);
+            this.btnProducts.Name = "btnProducts";
+            this.btnProducts.Size = new System.Drawing.Size(150, 40);
+            this.btnProducts.TabIndex = 5;
+            this.btnProducts.TextColor = System.Drawing.Color.White;
+            this.btnProducts.UseVisualStyleBackColor = false;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.panelLogo.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.panelLogo.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.panelLogo.BorderRadius = 0;
+            this.panelLogo.BorderSize = 0;
+            this.panelLogo.ForeColor = System.Drawing.Color.White;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(150, 40);
+            this.panelLogo.TabIndex = 6;
+            this.panelLogo.TextColor = System.Drawing.Color.White;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::QuanLyShipper.Properties.Resources.QLSP;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 36);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(80, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin";
+            // 
+            // QuanLy
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "QuanLy";
+            this.Load += new System.EventHandler(this.QuanLy_Load);
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private QuanLyShipper.RJControls.RJPanel panelDesktop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnCloseChildForm;
+        private QuanLyShipper.RJControls.RJPanel panelTitleBar;
+        private QuanLyShipper.RJControls.RJPanel panelLogo;
+        private QuanLyShipper.RJControls.RJLogin btnProducts;
+        private QuanLyShipper.RJControls.RJLogin btnQLShipper;
+        private QuanLyShipper.RJControls.RJLogin btnPBShipper;
+        private QuanLyShipper.RJControls.RJLogin btnThongBao;
+        private QuanLyShipper.RJControls.RJLogin btnCaiDat;
+        private QuanLyShipper.RJControls.RJLogin btn_dangxuat;
+        private QuanLyShipper.RJControls.RJPanel panelMenu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ formshipper/Forms/frm_VanChuyen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyShipper.formshipper.Forms
+{
+    public partial class frm_VanChuyen : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        frm_DonHang DH = new frm_DonHang();
+        public frm_VanChuyen()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void frm_VanChuyen_Resize(object sender, EventArgs e)
+        {
+            FormCollection fc = Application.OpenForms;
+            foreach (Form f in fc)
+            {
+                if (f.Name == "frm_QuanLyKhoaHoc")
+                {
+                    f.Height = DonHangDaNhan_dgv.Height;
+                    f.Width = DonHangDaNhan_dgv.Width;
+                }
+            }
+           
+        }
+
+
+        private void frm_VanChuyen_Load(object sender, EventArgs e)
+        {
+           
+        }
+            private List<object[]> _selectedRows;
+        public void frm_VanChuyen_Don(List<object[]> selectedRows)
+        {
+            // Loop qua tất cả các cột của DataGridView1 và sao chép vào DataGridView2
+            
+            InitializeComponent();
+
+            _selectedRows = selectedRows;
+
+            foreach (object[] row in selectedRows)
+            {
+                DonHangDaNhan_dgv.Rows.Add(row);
+            }
+
+        }
+    }
+}
+
+
