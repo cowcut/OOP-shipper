8f1c78a5f3504b26bfba7886714ec5dbd3cfb742
Duong DD <92495354+FE-BAPPP@users.noreply.github.com>
2023-05-31 16:18:16 +0700

    Add files via upload

---
+++ QuanLy/Forms/FormPBShipper.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace FormMainMenu.Forms
+{
+    partial class FormPBShipper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rjPanel1 = new QuanLyShipper.RJControls.RJPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtMarkerLabel = new System.Windows.Forms.TextBox();
+            this.btnClearMarkers = new QuanLyShipper.RJControls.RJLogin();
+            this.PnlMaSP = new System.Windows.Forms.Panel();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.lblCenterCoordinates = new System.Windows.Forms.Label();
+            this.mapContainer = new System.Windows.Forms.Panel();
+            this.btnAddMarker = new QuanLyShipper.RJControls.RJLogin();
+            this.panel1.SuspendLayout();
+            this.rjPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.PnlMaSP.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.rjPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1287, 703);
+            this.panel1.TabIndex = 1;
+            // 
+            // rjPanel1
+            // 
+            this.rjPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.rjPanel1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.rjPanel1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjPanel1.BorderRadius = 20;
+            this.rjPanel1.BorderSize = 0;
+            this.rjPanel1.Controls.Add(this.panel3);
+            this.rjPanel1.Controls.Add(this.btnClearMarkers);
+            this.rjPanel1.Controls.Add(this.PnlMaSP);
+            this.rjPanel1.Controls.Add(this.panel2);
+            this.rjPanel1.Controls.Add(this.lblCenterCoordinates);
+            this.rjPanel1.Controls.Add(this.mapContainer);
+            this.rjPanel1.Controls.Add(this.btnAddMarker);
+            this.rjPanel1.ForeColor = System.Drawing.Color.White;
+            this.rjPanel1.Location = new System.Drawing.Point(-1, 4);
+            this.rjPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.rjPanel1.Name = "rjPanel1";
+            this.rjPanel1.Size = new System.Drawing.Size(1288, 695);
+            this.rjPanel1.TabIndex = 0;
+            this.rjPanel1.TextColor = System.Drawing.Color.White;
+            this.rjPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.rjPanel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel3.Controls.Add(this.txtMarkerLabel);
+            this.panel3.Location = new System.Drawing.Point(591, 27);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.panel3.Size = new System.Drawing.Size(148, 32);
+            this.panel3.TabIndex = 4;
+            // 
+            // txtMarkerLabel
+            // 
+            this.txtMarkerLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMarkerLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMarkerLabel.Location = new System.Drawing.Point(0, 0);
+            this.txtMarkerLabel.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMarkerLabel.Multiline = true;
+            this.txtMarkerLabel.Name = "txtMarkerLabel";
+            this.txtMarkerLabel.Size = new System.Drawing.Size(148, 28);
+            this.txtMarkerLabel.TabIndex = 0;
+            // 
+            // btnClearMarkers
+            // 
+            this.btnClearMarkers.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnClearMarkers.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnClearMarkers.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnClearMarkers.BorderRadius = 10;
+            this.btnClearMarkers.BorderSize = 0;
+            this.btnClearMarkers.FlatAppearance.BorderSize = 0;
+            this.btnClearMarkers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClearMarkers.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearMarkers.ForeColor = System.Drawing.Color.White;
+            this.btnClearMarkers.Location = new System.Drawing.Point(832, 276);
+            this.btnClearMarkers.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClearMarkers.Name = "btnClearMarkers";
+            this.btnClearMarkers.Size = new System.Drawing.Size(159, 39);
+            this.btnClearMarkers.TabIndex = 9;
+            this.btnClearMarkers.Text = "Xóa";
+            this.btnClearMarkers.TextColor = System.Drawing.Color.White;
+            this.btnClearMarkers.UseVisualStyleBackColor = false;
+            this.btnClearMarkers.Click += new System.EventHandler(this.btnClearMarkers_Click);
+            // 
+            // PnlMaSP
+            // 
+            this.PnlMaSP.BackColor = System.Drawing.Color.Gainsboro;
+            this.PnlMaSP.Controls.Add(this.txtLatitude);
+            this.PnlMaSP.Location = new System.Drawing.Point(92, 27);
+            this.PnlMaSP.Margin = new System.Windows.Forms.Padding(0);
+            this.PnlMaSP.Name = "PnlMaSP";
+            this.PnlMaSP.Padding = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.PnlMaSP.Size = new System.Drawing.Size(148, 32);
+            this.PnlMaSP.TabIndex = 3;
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLatitude.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLatitude.Location = new System.Drawing.Point(0, 0);
+            this.txtLatitude.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLatitude.Multiline = true;
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.Size = new System.Drawing.Size(148, 28);
+            this.txtLatitude.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.txtLongitude);
+            this.panel2.Location = new System.Drawing.Point(348, 27);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.panel2.Size = new System.Drawing.Size(148, 32);
+            this.panel2.TabIndex = 10;
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLongitude.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLongitude.Location = new System.Drawing.Point(0, 0);
+            this.txtLongitude.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLongitude.Multiline = true;
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.Size = new System.Drawing.Size(148, 28);
+            this.txtLongitude.TabIndex = 0;
+            // 
+            // lblCenterCoordinates
+            // 
+            this.lblCenterCoordinates.AutoSize = true;
+            this.lblCenterCoordinates.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCenterCoordinates.Location = new System.Drawing.Point(828, 16);
+            this.lblCenterCoordinates.Name = "lblCenterCoordinates";
+            this.lblCenterCoordinates.Size = new System.Drawing.Size(152, 23);
+            this.lblCenterCoordinates.TabIndex = 8;
+            this.lblCenterCoordinates.Text = "Tọa độ trung tâm";
+            // 
+            // mapContainer
+            // 
+            this.mapContainer.Location = new System.Drawing.Point(30, 64);
+            this.mapContainer.Name = "mapContainer";
+            this.mapContainer.Size = new System.Drawing.Size(767, 607);
+            this.mapContainer.TabIndex = 7;
+            this.mapContainer.SizeChanged += new System.EventHandler(this.FormPBShipper_Load);
+            // 
+            // btnAddMarker
+            // 
+            this.btnAddMarker.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnAddMarker.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnAddMarker.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnAddMarker.BorderRadius = 10;
+            this.btnAddMarker.BorderSize = 0;
+            this.btnAddMarker.FlatAppearance.BorderSize = 0;
+            this.btnAddMarker.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddMarker.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMarker.ForeColor = System.Drawing.Color.White;
+            this.btnAddMarker.Location = new System.Drawing.Point(832, 154);
+            this.btnAddMarker.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddMarker.Name = "btnAddMarker";
+            this.btnAddMarker.Size = new System.Drawing.Size(159, 39);
+            this.btnAddMarker.TabIndex = 6;
+            this.btnAddMarker.Text = "Thêm";
+            this.btnAddMarker.TextColor = System.Drawing.Color.White;
+            this.btnAddMarker.UseVisualStyleBackColor = false;
+            this.btnAddMarker.Click += new System.EventHandler(this.btnAddMarker_Click);
+            // 
+            // FormPBShipper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 703);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormPBShipper";
+            this.Text = "Phân bổ Shipper";
+            this.Load += new System.EventHandler(this.FormPBShipper_Load);
+            this.panel1.ResumeLayout(false);
+            this.rjPanel1.ResumeLayout(false);
+            this.rjPanel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.PnlMaSP.ResumeLayout(false);
+            this.PnlMaSP.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private QuanLyShipper.RJControls.RJPanel rjPanel1;
+        private QuanLyShipper.RJControls.RJLogin btnAddMarker;
+        private System.Windows.Forms.Panel mapContainer;
+        private QuanLyShipper.RJControls.RJLogin btnClearMarkers;
+        private System.Windows.Forms.Label lblCenterCoordinates;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtMarkerLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.Panel PnlMaSP;
+        private System.Windows.Forms.TextBox txtLatitude;
+    }
+}
---
+++ QuanLy/Forms/FormThongBao.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMainMenu.Forms
+{
+    public partial class FormThongBao : Form
+    {
+        public FormThongBao()
+        {
+            InitializeComponent();
+        }
+        private void LayDSHang()
+        {
+            //khởi tạo các đối tượng SqlConnection, SqlDataAdapter, DataTable
+            SqlConnection conn = new SqlConnection();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+            //lấy chuỗi kết nối từ file App.config
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["QLHangEntities"].ConnectionString;
+
+            try
+            {
+                //mở chuỗi kết nối
+                conn.Open();
+                //khai báo đối tượng SqlCommand trong SqlDataAdapter
+                da.SelectCommand = new SqlCommand();
+                //gọi thủ tục từ SQL
+                da.SelectCommand.CommandText = "SP_Retrieve_User";
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                //gán chuỗi kết nối
+                da.SelectCommand.Connection = conn;
+                //sử dụng phương thức fill để điền dữ liệu từ datatable vào SqlDataAdapter
+                da.Fill(dt);
+                //gán dữ liệu từ datatable vào datagridview
+                DGVMatHang.DataSource = dt;
+                //đóng chuỗi kết nối
+                conn.Close();
+                //sử dụng thuộc tính Width và HeaderText để set chiều dài và tiêu đề cho các coloumns
+                DGVMatHang.Columns[0].Width = 170;
+                DGVMatHang.Columns[0].HeaderText = "Mã Sản Phẩm";
+                DGVMatHang.Columns[1].Width = 170;
+                DGVMatHang.Columns[1].HeaderText = "Tên Sản Phẩm";
+                DGVMatHang.Columns[2].Width = 170;
+                DGVMatHang.Columns[2].HeaderText = "Giá Sản Phẩm";
+                DGVMatHang.Columns[3].Width = 170;
+                DGVMatHang.Columns[3].HeaderText = "Loại Sản Phẩm";
+                DGVMatHang.Columns[4].Width = 180;
+                DGVMatHang.Columns[4].HeaderText = "Địa Chỉ";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btn_HienThi_Click(object sender, EventArgs e)
+        {
+            LayDSHang();
+        }
+        private void DGVMatHang_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < DGVMatHang.Rows.Count)
+            {
+                DataGridViewRow row = DGVMatHang.Rows[e.RowIndex];
+                txtMaSP.Text = Convert.ToString(row.Cells["MaSP"].Value);
+                txtTenSP.Text = Convert.ToString(row.Cells["TenSP"].Value);
+                txtGiaSP.Text = Convert.ToString(row.Cells["GiaSP"].Value);
+                txtLoaiSP.Text = Convert.ToString(row.Cells["LoaiSP"].Value);
+                txtDiaChi.Text = Convert.ToString(row.Cells["DiaChi"].Value);
+            }
+        }
+
+        public bool KiemTraThongTin()
+        {
+
+            if (txtTenSP.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập tên sản phẩm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTenSP.Focus();
+                return false;
+            }
+            if (txtGiaSP.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập giá sản phẩm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtGiaSP.Focus();
+                return false;
+            }
+            if (txtLoaiSP.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập loại sản phẩm ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtLoaiSP.Focus();
+                return false;
+            }
+            if (txtDiaChi.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập địa chỉ nhà sản xuất", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDiaChi.Focus();
+                return false;
+            }
+            return true;
+        }
+        private void txt_TimKiem_TextChanged(object sender, EventArgs e)
+        {
+            string keyword = txt_TimKiem.Text.ToLower();
+            LayDSHang();
+            foreach (DataGridViewRow row in DGVMatHang.Rows)
+            {
+                string tenSP = row.Cells["TenSP"].Value.ToString().ToLower();
+
+                if (tenSP.Contains(keyword))
+                {
+                    row.Visible = true; // Hiển thị dòng nếu tìm thấy từ khóa
+                }
+                else
+                {
+                    row.Visible = false; // Ẩn dòng nếu không tìm thấy từ khóa
+                }
+            }
+        }
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            if (KiemTraThongTin())
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection();
+                    conn.ConnectionString = ConfigurationManager.ConnectionStrings["QLHangEntities"].ConnectionString;
+                    SqlCommand cmd = new SqlCommand();
+
+                    cmd.CommandText = "SP_ThemMatHang";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@TenSP", SqlDbType.NVarChar).Value = txtTenSP.Text;
+                    cmd.Parameters.Add("@GiaSP", SqlDbType.NVarChar).Value = txtGiaSP.Text;
+                    cmd.Parameters.Add("@LoaiSP", SqlDbType.NVarChar).Value = txtLoaiSP.Text;
+                    cmd.Parameters.Add("@DiaChi", SqlDbType.NVarChar).Value = txtDiaChi.Text;
+
+
+
+                    cmd.Connection = conn;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    LayDSHang();
+                    Reset();
+                    MessageBox.Show("Đã thêm mới mặt hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            if (txtMaSP.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập mã sản phẩm cần sửa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaSP.Focus();
+            }
+            else if (KiemTraThongTin())
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection();
+                    conn.ConnectionString = ConfigurationManager.ConnectionStrings["QLHangEntities"].ConnectionString;
+                    SqlCommand cmd = new SqlCommand();
+
+                    cmd.CommandText = "SP_SuaMatHang";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@MaSP", SqlDbType.Int).Value = Convert.ToInt32(txtMaSP.Text);
+                    cmd.Parameters.Add("@TenSP", SqlDbType.NVarChar).Value = txtTenSP.Text;
+                    cmd.Parameters.Add("@GiaSP", SqlDbType.NVarChar).Value = txtGiaSP.Text;
+                    cmd.Parameters.Add("@LoaiSP", SqlDbType.NVarChar).Value = txtLoaiSP.Text;
+                    cmd.Parameters.Add("@DiaChi", SqlDbType.NVarChar).Value = txtDiaChi.Text;
+
+                    cmd.Connection = conn;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    LayDSHang();
+                    Reset();
+                    MessageBox.Show("Đã sửa mặt hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            if (txtMaSP.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập mã mặt hàng cần xóa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaSP.Focus();
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection();
+                    conn.ConnectionString = ConfigurationManager.ConnectionStrings["QLHangEntities"].ConnectionString;
+                    SqlCommand cmd = new SqlCommand();
+
+                    cmd.CommandText = "SP_XoaMatHang";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@MaSP", SqlDbType.Int).Value = Convert.ToInt32(txtMaSP.Text);
+                    cmd.Connection = conn;
+
+                    conn.Open();
+
+                    DialogResult dg = MessageBox.Show("Bạn có chắn chắn muốn xóa?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dg == DialogResult.Yes)
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    conn.Close();
+
+                    LayDSHang();
+                    Reset();
+                    MessageBox.Show("Đã xóa mặt hàng thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        public void Reset()
+        {
+            txtMaSP.Text = "";
+            txtTenSP.Text = "";
+            txtGiaSP.Text = "";
+            txtLoaiSP.Text = "";
+            txtDiaChi.Text = "";
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            //sử dụng DialogResult để kiểm tra kết quả trả về và hiển thị hộp thoại thông báo
+            DialogResult dg = MessageBox.Show("Bạn có muốn thoát?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dg == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTenSP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DGVShipper_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void rjPanel2_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void rjPanel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
