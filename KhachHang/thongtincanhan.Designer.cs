20f58238de57a902c7ced5924a3cb2bce44a3191
cowcut <112707238+cowcut@users.noreply.github.com>
2023-06-01 21:56:29 +0700

    Add files via upload

---
+++ KhachHang/thongtincanhan.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace KhachHang
+{
+    partial class thongtincanhan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TenHienThi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.MatKhau = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.MatKhauMoi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.NhapLai = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.update = new System.Windows.Forms.Button();
+            this.thoat = new System.Windows.Forms.Button();
+            this.iconToolStripButton1 = new FontAwesome.Sharp.IconToolStripButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Email);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(9, 21);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(442, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // Email
+            // 
+            this.Email.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Email.Location = new System.Drawing.Point(210, 22);
+            this.Email.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(222, 26);
+            this.Email.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên tài khoản:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.TenHienThi);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(9, 96);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(442, 67);
+            this.panel2.TabIndex = 1;
+            // 
+            // TenHienThi
+            // 
+            this.TenHienThi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.TenHienThi.Location = new System.Drawing.Point(225, 19);
+            this.TenHienThi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TenHienThi.Name = "TenHienThi";
+            this.TenHienThi.Size = new System.Drawing.Size(207, 26);
+            this.TenHienThi.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ngày, tháng, năm sinh :";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.MatKhau);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(9, 167);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(442, 67);
+            this.panel3.TabIndex = 2;
+            // 
+            // MatKhau
+            // 
+            this.MatKhau.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.MatKhau.Location = new System.Drawing.Point(210, 22);
+            this.MatKhau.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MatKhau.Name = "MatKhau";
+            this.MatKhau.Size = new System.Drawing.Size(222, 26);
+            this.MatKhau.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Địa chỉ nhà :";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.MatKhauMoi);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(9, 239);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(442, 67);
+            this.panel4.TabIndex = 3;
+            // 
+            // MatKhauMoi
+            // 
+            this.MatKhauMoi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.MatKhauMoi.Location = new System.Drawing.Point(160, 22);
+            this.MatKhauMoi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MatKhauMoi.Name = "MatKhauMoi";
+            this.MatKhauMoi.Size = new System.Drawing.Size(272, 26);
+            this.MatKhauMoi.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 19);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Số điện thoại :";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.NhapLai);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(9, 318);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(442, 67);
+            this.panel5.TabIndex = 4;
+            // 
+            // NhapLai
+            // 
+            this.NhapLai.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.NhapLai.Location = new System.Drawing.Point(160, 22);
+            this.NhapLai.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.NhapLai.Name = "NhapLai";
+            this.NhapLai.Size = new System.Drawing.Size(272, 26);
+            this.NhapLai.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 19);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 22);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Email :";
+            // 
+            // update
+            // 
+            this.update.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.update.Location = new System.Drawing.Point(275, 409);
+            this.update.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(81, 28);
+            this.update.TabIndex = 5;
+            this.update.Text = "Cập nhật";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // thoat
+            // 
+            this.thoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.thoat.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.thoat.Location = new System.Drawing.Point(370, 409);
+            this.thoat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.thoat.Name = "thoat";
+            this.thoat.Size = new System.Drawing.Size(81, 28);
+            this.thoat.TabIndex = 6;
+            this.thoat.Text = "Thoát";
+            this.thoat.UseVisualStyleBackColor = true;
+            this.thoat.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // iconToolStripButton1
+            // 
+            this.iconToolStripButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconToolStripButton1.IconColor = System.Drawing.Color.Black;
+            this.iconToolStripButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconToolStripButton1.Name = "iconToolStripButton1";
+            this.iconToolStripButton1.Size = new System.Drawing.Size(23, 23);
+            this.iconToolStripButton1.Text = "iconToolStripButton1";
+            // 
+            // thongtincanhan
+            // 
+            this.AcceptButton = this.update;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(111)))), ((int)(((byte)(246)))));
+            this.CancelButton = this.thoat;
+            this.ClientSize = new System.Drawing.Size(484, 474);
+            this.Controls.Add(this.thoat);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "thongtincanhan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chỉnh sửa thông tin cá nhân";
+            this.Load += new System.EventHandler(this.thongtincanhan_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TenHienThi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox MatKhau;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox MatKhauMoi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox NhapLai;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button thoat;
+        private FontAwesome.Sharp.IconToolStripButton iconToolStripButton1;
+    }
+}
